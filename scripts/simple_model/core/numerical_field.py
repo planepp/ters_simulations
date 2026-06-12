21cde92d3ff1fb46a3f3cf52b8918c24e75947d6
Planelles Prensa Pablo <pabloplanellesp@gmail.com>
2026-06-12 16:17:24 +0300

    unsuccessful numerical field testing

--- scripts/simple_model/core/numerical_field.py
+++ scripts/simple_model/core/numerical_field.py
@@ -1,5 +1,5 @@
 import numpy as np
-def numerical_field(atom_polarizabilities, atoms, frequencies, E_loc):
+def numerical_field(atom_polarizabilities, atoms, frequencies, E_loc, N1):
     """
     Calculates and returns intensities of each vibrational mode and polarized dipoles of each 
     atom for each vibrational mode, given an externally provided local field.
@@ -16,8 +16,12 @@ def numerical_field(atom_polarizabilities, atoms, frequencies, E_loc):
     """
     E_local = E_loc.T.reshape(3*atoms)
 
-    # Calculating mode intensities and dipoles
-    mode_intensities = np.zeros((3*atoms-6, 2))
+     # Calculating mode intensities and dipoles
+    if N1 == 2:
+        n_modes = 3*N1 - 5    
+    else:
+        n_modes = 3*N1 - 6
+    mode_intensities = np.zeros((n_modes, 2))
     dipoles = np.tensordot(atom_polarizabilities, E_local, axes=([2], [0]))
     E_scattering = np.einsum('i,ij->j', E_local, dipoles.T)
 
