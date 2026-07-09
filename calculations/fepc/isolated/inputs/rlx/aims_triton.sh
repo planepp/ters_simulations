bd5da121205a27fda2605bd88a91ba0915ceae34
Planelles Prensa Pablo <planelp1@login4.triton.aalto.fi>
2026-07-09 17:07:54 +0300

    add fepc-triton

---
+++ calculations/fepc/isolated/inputs/rlx/aims_triton.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J test
+#SBATCH -o out/%j.out
+#SBATCH --time=13:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem-per-cpu=2000
+
+module purge
+module load triton/2024.1-gc aims/250320
+export OMP_NUM_THREADS=1
+ulimit -s unlimited
+
+AIMS_BIN="aims.250320.scalapack.mpi.x"
+srun $AIMS_BIN > aims.out
