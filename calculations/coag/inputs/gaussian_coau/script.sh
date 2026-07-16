f82d4e303d3bfa06ab517ca014a948ba6e7cd5fb
Planelles Prensa Pablo <planelp1@login4.triton.aalto.fi>
2026-07-16 14:51:34 +0300

    co to coag

---
+++ calculations/coag/inputs/gaussian_coau/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -J gaussian
+#SBATCH -o gaussian.out
+#SBATCH -e gaussian.err
+#SBATCH -t 36:00:00
+#SBATCH --account=project_2001912
+#SBATCH -N 1
+#SBATCH -c 24
+
+module purge
+module load gaussian anaconda
+export g16root=/appl/soft/chem/gaussian/G16RevC.02
+source $g16root/g16/bsd/g16.profile
+export OMP_NUM_THREADS=1
+export GAUSS_SCRDIR=$PWD/$SLURM_JOB_ID.GAUSS_SCRDIR
+mkdir -p $GAUSS_SCRDIR
+lfs setstripe -S4M -E 64M -c 1 -E 2G -c 4 -E -1 -c 8 $GAUSS_SCRDIR
+
+#python3 teste.py > output
+srun g16 <auco.com>& auco.log 
+
+cleanup() {
+  rm -rf $GAUSS_SCRDIR
+}
+trap cleanup 0
---
+++ calculations/coag/inputs/images/readme.md
@@ -0,0 +1,2 @@
+Input geometry.for images = Ouput geometryfin.in for rlx
+Additional inputs for images = Output for modes
